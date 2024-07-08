da297da10865000099aaca54887cbe41fbbd2f3a
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-07-08 23:19:14 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const grab=document.getElementById('container')
+const newHeader= document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent='Alvin is the champion'
